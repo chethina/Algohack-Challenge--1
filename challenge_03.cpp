6ddb605495bad7e1f7b5296dc8fd1e520296de7e
Chethina Randidu <lgrandiduchethina@gmail.com>
2023-10-19 16:08:36 +0530

    Create challenge_03.cpp

---
+++ challenge_03.cpp
@@ -0,0 +1,17 @@
+//3) Once upon a time, there were three little crows.
+//They were Lady Crow, Lord Crow, and Sister Crow.
+//Lady Crow was 5 years old. Lord Crow was 4 years old and Sister Crow was 3 years old. What was their total age?
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int lady_c=5,lord_c=4,sister_c=3;
+    int total = lady_c+lord_c+sister_c;
+
+    cout << "3) Once upon a time, there were three little crows. They were Lady Crow, Lord Crow, and Sister Crow. Lady Crow was 5 years old. Lord Crow was 4 years old and Sister Crow was 3 years old. What was their total age?" << endl;
+    cout << "Ages, " << endl << "Lady Crow : " << lady_c << endl << "Lord Crow : " << lord_c << endl << "Sister Crow : " << sister_c << endl;
+    cout << "Total of there ages : " << total;
+
+
+}
